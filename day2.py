11abb61e47532700aaa486c98c5c00acb960aec2
Manjunath <hanamanthgad123@gmail.com>
2022-01-06 17:50:18 +0530

    added day1,day2 notebook and python files

---
+++ day2.py
@@ -0,0 +1,8 @@
+class Layer_Dense:
+
+def __init__(self, n_inputs, n_neurons) :
+# Initialize weights and biases
+pass # using pass statement as a placeholder
+      # Forward pass
+def forward(self, inputs):
+# Calculate output values from inputs, weights and biases ​pass ​# using pass statement as a placeholder
